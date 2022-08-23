f2b0092e811ca303986834a4bc6a5073891cc17b
ja1dan <ajaidan0@gmail.com>
2022-08-22 22:52:21 -0400

    fix: cors

--- source/main.ts
+++ source/main.ts
@@ -2,17 +2,15 @@
 import { infoRoute } from './routes/info.js'
 import { installRoute } from './routes/installs.js'
 import { statusRoute } from './routes/status.js'
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import pino from 'pino'
 
 // create server
 const app = fastify()
 const port = 8543
-
-app.all('/*', (request, reply) => {
-    reply.header('Access-Control-Allow-Origin', '*')
-    reply.header('Access-Control-Allow-Headers', 'X-Requested-With')
-})
+// cors
+app.register(cors, {'origin': true})
 
 app.register(infoRoute)
 app.register(installRoute)
