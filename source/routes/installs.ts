86b567ac100528a116894dbb3815624d2eca5549
iCrazeiOS <icrazeios@protonmail.com>
2022-10-07 20:55:03 +0100

    tmp /troll endpoint for trollinstaller 2

--- source/routes/installs.ts
+++ source/routes/installs.ts
@@ -23,6 +23,13 @@ export const installRoute = (fastify, opts, next) => {
         }
     )
 
+    // just for TrollInstaller 2
+    // will add "normally" once it supports arm64 devices
+    fastify.get('/troll', (request: FastifyRequest, reply: FastifyReply) => {
+        return reply.redirect("itms-services://?action=download-manifest&url=https://jailbreaks.app/cdn/plists/TrollInstaller2.plist");
+    }
+)
+
     // move to next route
     next()
 }
